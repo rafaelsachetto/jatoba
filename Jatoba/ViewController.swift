558771a6b54f5e8324b5093a3850aa133c2bb944
Rafael Sachetto <rafael@sachetto.com.br>
2021-04-07 17:16:18 -0300

    feat: Adds LayoutInspector

--- Jatoba/ViewController.swift
+++ Jatoba/ViewController.swift
@@ -8,12 +8,12 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var messageLabel: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        messageLabel.text = "Hello world!"
     }
-
-
 }
 
